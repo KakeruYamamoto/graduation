d84e078922e10b5e640b4c47e0402ea638b09d51
kakeru <okinawan.kkr0713@icloud.com>
2019-12-25 05:18:20 +0900

    routes.rbの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'events#index'
   resources :events do
     collection do
       post :confirm
     end
+  end
+
+  if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 end
