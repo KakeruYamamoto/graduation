73afcfb5628e51e8c853037d402fded5df5fd96f
kakeru <okinawan.kkr0713@icloud.com>
2019-12-25 06:13:01 +0900

    before_action追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, only: [:new, :create]
   def index
     @events = Event.all
   end
