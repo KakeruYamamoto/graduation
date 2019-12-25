89eafd91b24dc6977b4769faa29a49e9177c020d
kakeru <okinawan.kkr0713@icloud.com>
2019-12-25 11:26:04 +0900

    class Ability　編集

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    if user.try(:admin?)
+      can :access, :rails_admin
+      can :manage, :all
+    end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
