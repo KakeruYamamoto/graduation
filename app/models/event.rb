a81335e0f16b9cdc8e7a60fc80cd05d862492956
kakeru <okinawan.kkr0713@icloud.com>
2019-12-25 10:57:19 +0900

    eventモデルのアソシエーション

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   # has_many :favorites, dependent: :destroy
   # has_many :favorite_users, through: :favorites, source: :user
   mount_uploader :image, ImageUploader
-  # belongs_to :user
+  belongs_to :user
   validates :content,  presence: true
   validates :title,  presence: true
 end
