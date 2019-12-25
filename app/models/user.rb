d3b54279a69f25a84ab3e513e1892fbac43928bc
kakeru <okinawan.kkr0713@icloud.com>
2019-12-25 11:08:33 +0900

    userモデルのアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   mount_uploader :icon, ImageUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
+  has_many :feeds
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
