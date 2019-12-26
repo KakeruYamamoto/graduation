e8dc64ea7d1f8067dbd947a352567912beab502f
kakeru <okinawan.kkr0713@icloud.com>
2019-12-26 10:51:12 +0900

    userモデル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
   mount_uploader :icon, ImageUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :feeds
+  has_many :events
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email,presence: true, length: { maximum: 255 }, uniqueness: true,
+                format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
