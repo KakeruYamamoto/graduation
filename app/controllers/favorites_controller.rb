6d68c3e39b3753f6a36d4609ddc858322bc66a36
kakeru <okinawan.kkr0713@icloud.com>
2019-12-28 21:13:44 +0900

    controller favorites 編集

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,12 @@
 class FavoritesController < ApplicationController
+
+  def create
+    favorite = current_user.favorites.create(event_id: params[:event_id])
+    redirect_to events_url, notice: "#{favorite.event.user.name}さんのブログをお気に入り登録しました"
+  end
+
+  def destroy
+    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    redirect_to events_url, notice: "#{favorite.event.user.name}さんのブログをお気に入り解除しました"
+  end
 end
