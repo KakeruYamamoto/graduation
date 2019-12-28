57391f1ee24dd4f81ec765bfeee5fe9d04e36a6a
kakeru <okinawan.kkr0713@icloud.com>
2019-12-28 20:54:54 +0900

    モデルにアソシエーション

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,7 @@ class Event < ApplicationRecord
   # has_many :favorite_users, through: :favorites, source: :user
   mount_uploader :image, ImageUploader
   belongs_to :user, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   validates :content,  presence: true
   validates :title,  presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   mount_uploader :icon, ImageUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :favorites, dependent: :destroy
   has_many :events
   validates :name, presence: true, length: { maximum: 30 }
   validates :email,presence: true, length: { maximum: 255 }, uniqueness: true,
