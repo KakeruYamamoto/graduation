ba05d5d67ea22767a1a286884fbfe115498fcb3c
kakeru <okinawan.kkr0713@icloud.com>
2019-12-28 21:19:16 +0900

    controller favorites 再編集

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,11 +2,11 @@ class FavoritesController < ApplicationController
 
   def create
     favorite = current_user.favorites.create(event_id: params[:event_id])
-    redirect_to events_url, notice: "#{favorite.event.user.name}さんのブログをお気に入り登録しました"
+    redirect_to events_url, notice: "#{favorite.event.user.name}さんのイベントをあとで見るリストしました"
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to events_url, notice: "#{favorite.event.user.name}さんのブログをお気に入り解除しました"
+    redirect_to events_url, notice: "#{favorite.event.user.name}さんのイベントをあとで見るリストから解除しました"
   end
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,4 @@
 class Favorite < ApplicationRecord
   belongs_to :user
-  belongs_to :blog
+  belongs_to :event
 end
