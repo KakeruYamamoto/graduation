4be1df9e477844f7b5824b1b2105f72575ab6be9
kakeru <okinawan.kkr0713@icloud.com>
2020-01-06 17:08:46 +0900

    表示の並び修正

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,15 +3,15 @@ class EventsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
 
   def index
-    @events = Event.order(created_at: :desc).page(params[:page]).per(20)
+    @events = Event.order(e_date_start: :desc).page(params[:page]).per(20)
 
     if params[:q] != nil
       params[:q]['title_or_content_address_cont_any'] = params[:q]['title_or_content_address_cont_any'].split(/[\p{blank}\s]+/)
       @q = Event.ransack(params[:q])
-      @events = @q.result.page(params[:page]).per(20)
+      @events = @q.result.order(e_date_start: :desc).page(params[:page]).per(20)
     else
       @q = Event.ransack(params[:q])
-      @events = @q.result(distinct: true).page(params[:page]).per(20)
+      @events = @q.result(distinct: true).order(e_date_start: :desc).page(params[:page]).per(20)
     end
   end
 
