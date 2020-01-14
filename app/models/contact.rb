e5c4e982a64f6ce5f21be4f85b345d833d8ebefc
kakeru <okinawan.kkr0713@icloud.com>
2020-01-14 19:46:57 +0900

    contactモデルのテスト実装

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ApplicationRecord
   belongs_to :event
+  validates :title,  presence: true
+  validates :email,  presence: true
+  validates :content,  presence: true
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @user1 = FactoryBot.create(:first_user)
+    @event1 = FactoryBot.create(:event, user_id: @user1.id)
+  end
+
+  it "titleが空ならバリデーションが通らない" do
+    contact = Contact.new(title: '',email: "seikou@gmail.com", content: '失敗テスト', event_id: @event1.id)
+    expect(contact).not_to be_valid
+  end
+
+  it "emailが空ならバリデーションが通らない" do
+    contact = Contact.new(title: '失敗テスト', email: "", content: '', event_id: @event1.id)
+    expect(contact).not_to be_valid
+  end
+
+  it "contentが空ならバリデーションが通らない" do
+    contact = Contact.new(title: '失敗テスト',email: "seikou@gmail.com", content: '', event_id: @event1.id)
+    expect(contact).not_to be_valid
+  end
+
+  it "titleとemailとcontentに内容が記載されていればバリデーションが通る" do
+    contact = Contact.new(title: '成功テスト',email: "seikou@gmail.com", content: '成功テスト', event_id: @event1.id)
+    expect(contact).to be_valid
+  end
 end
