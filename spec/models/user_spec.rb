9956d5003b46fd785f4b497d3c1db940b30a3844
kakeru <okinawan.kkr0713@icloud.com>
2020-01-14 19:23:44 +0900

    userモデルのテスト実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "nameが空ならバリデーションが通らない" do
+    user = User.new(name: '', email: '失敗テスト', password: "111111")
+    expect(user).not_to be_valid
+  end
+
+  it "emailが空ならバリデーションが通らない" do
+    user = User.new(name: '失敗テスト', email: '', password: "111111")
+    expect(user).not_to be_valid
+  end
+
+  it "passwordが空ならバリデーションが通らない" do
+    user = User.new(name: '失敗テスト', email: 'test_user1@gmail.com', password: "")
+    expect(user).not_to be_valid
+  end
+
+
+  it "nameとemailとpasswordに内容が記載されていればバリデーションが通る" do
+    user = User.new(name: '成功テスト', email: 'test_user1@gmail.com', password: "111111")
+    expect(user).to be_valid
+  end
 end
