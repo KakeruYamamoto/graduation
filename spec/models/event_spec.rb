59792831493c42fb897923f404ef563854cda054
kakeru <okinawan.kkr0713@icloud.com>
2020-01-14 19:34:43 +0900

    eventモデルのテスト実装

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,22 @@
+
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user1 = FactoryBot.create(:first_user)
+  end
+  it "titleが空ならバリデーションが通らない" do
+  event = Event.new(title: '', content: '失敗テスト', user_id: @user1.id)
+  expect(event).not_to be_valid
+end
+
+it "contentが空ならバリデーションが通らない" do
+  event = Event.new(title: '失敗テスト', content: '', user_id: @user1.id)
+  expect(event).not_to be_valid
+end
+
+it "titleとcontentに内容が記載されていればバリデーションが通る" do
+  event = Event.new(title: '成功テスト', content: '成功テスト', user_id: @user1.id)
+  expect(event).to be_valid
+end
 end
