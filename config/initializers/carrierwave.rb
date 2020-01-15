294976b1e275dc28fca296119015c22e284b54d8
kakeru <okinawan.kkr0713@icloud.com>
2020-01-15 19:24:31 +0900

    config/initializers/carrierwave.rb 修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -18,10 +18,13 @@ CarrierWave.configure do |config|
 
   case Rails.env
     when 'production'
-      config.fog_directory = '本番環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/本番環境用のバケット名'
+      config.fog_directory = 'achieve-production123'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/achieve-production123'
     when 'development'
-      config.fog_directory = '開発環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/開発環境用のバケット名'
+      config.fog_directory = 'achieve-development123'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/achieve-development123'
+    when 'test'
+      config.fog_directory = 'achieve-test123'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/achieve-test123'
   end
 end
