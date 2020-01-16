71cd84b295e0a897296ee0c6c87c4a75a32f7d05
kakeru <okinawan.kkr0713@icloud.com>
2020-01-16 20:34:27 +0900

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.6.0'
 
 set :application, 'graduation'
 
-set :repo_url, 'https://github.com/KakeruYamamoto/GGraduation.git'
+set :repo_url, 'https://github.com/KakeruYamamoto/graduation.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
