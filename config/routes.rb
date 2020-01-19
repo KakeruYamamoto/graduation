e9a7c13b5e8f1de0e7c3f4fe29b7986c8fc45fa9
kakeru <okinawan.kkr0713@icloud.com>
2020-01-19 21:07:53 +0900

    routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,22 @@
 Rails.application.routes.draw do
-  resources :contacts, only: [:new, :create, :show, :destroy]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  resources :favorites, only: [:create, :destroy]
-  resources :parthicipant_managements, only: [:create, :destroy]
-  get 'users/show'
-  # get 'search', to: 'events#search'
-  # post '/events/guest_sign_in', to: 'events#new_guest'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
         registrations: 'users/registrations'
         }
-  resources :users, only: [:show]
   root to: 'events#index'
+  resources :users, only: [:show]
+  resources :contacts, only: [:new, :create, :show, :destroy]
+  resources :favorites, only: [:create, :destroy]
+  resources :parthicipant_managements, only: [:create, :destroy]
   resources :events do
     collection do
       post :confirm
-      # post :guest_sign_in, as: 'events#new_guest'
       post :new_guest,as:'guest_sign_in'
       get :favorite
       get :parthicipante_events
       get :search
     end
-    # collection do
-    #   get :favorite
-    # end
-    # collection do
-    #   get :parthicipante_events
-    # end
-    # collection do
-    #   post :new_guest
-    # end
   end
 
 end
