ec1767603b0ad182cd5f932d39c7b2a0ce5657c1
kakeru <okinawan.kkr0713@icloud.com>
2020-01-22 15:39:59 +0900

    eventのバリデーション

--- app/controllers/parthicipant_managements_controller.rb
+++ app/controllers/parthicipant_managements_controller.rb
@@ -1,5 +1,6 @@
 class ParthicipantManagementsController < ApplicationController
     before_action :authenticate_user!
+    before_action :ended_event,only: [:create]
   def create
     parthicipante = current_user.parthicipant_managements.create(event_id: params[:event_id])
     redirect_to events_url, notice: "[#{parthicipante.event.title}]の参加しました"
@@ -9,4 +10,12 @@ class ParthicipantManagementsController < ApplicationController
     parthicipante = current_user.parthicipant_managements.find_by(id: params[:id]).destroy
     redirect_to events_url, notice: "[#{parthicipante.event.title}]の参加をキャンセルしました"
   end
+
+private
+
+  def ended_event
+    event_date = Event.find(params[:event_id].to_i).e_date_start if params[:event_id]
+    redirect_to events_path, notice: "このイベントは終了しています。" if event_date <= Time.now
+  end
+
 end
--- app/models/parthicipant_management.rb
+++ app/models/parthicipant_management.rb
@@ -1,4 +1,4 @@
 class ParthicipantManagement < ApplicationRecord
   belongs_to :user
-  belongs_to :event
+  belongs_to :event  
 end
