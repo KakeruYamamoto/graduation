25a3ad1b1e80a0a5b8f87611f9442b598efec663
kakeru <okinawan.kkr0713@icloud.com>
2020-03-12 19:30:11 +0900

    ユーザページ修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,11 +2,11 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user!
   def create
     favorite = current_user.favorites.create(event_id: params[:event_id])
-    redirect_to events_url, notice: "[#{favorite.event.title}]のイベントをあとで見るリストしました"
+    redirect_to events_url, notice: "[#{favorite.event.title}]をあとで見るリストに追加しました"
   end
 
   def destroy
     favorite = current_user.favorites.find(params[:id]).destroy
-    redirect_to events_url, notice: "[#{favorite.event.title}]のイベントをあとで見るリストから解除しました"
+    redirect_to events_url, notice: "[#{favorite.event.title}]をあとで見るリストから削除しました"
   end
 end
